bd376f6351b60e6ac6823ff3939f0bdbd39926d1
txqcoder <17693430686@163.com>
2022-04-02 17:58:07 +0800

    init

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/menu.svg"),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset("assets/icons/Location.svg"),
+              const SizedBox(width: defaultPadding / 2),
+              Text(
+                "15/2 New Texas",
+                style: Theme.of(context).textTheme.subtitle2,
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset("assets/icons/Notification.svg"),
+            )
+          ],
+        ),
+        body: Column(children: [
+          Text(
+            "Explore",
+            style: Theme.of(context)
+                .textTheme
+                .headline4!
+                .copyWith(fontWeight: FontWeight.w500, color: Colors.black),
+          )
+        ]));
+  }
+}
